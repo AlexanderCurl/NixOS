621d9e7ccc9b43726d99fd8c1d6d1fa0258eeaab
AlexanderCurl <alexc@alexc.hu>
2026-07-24 12:12:58 +0100

    Update

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,11 +1,6 @@
 { config, pkgs, inputs, ... }:
 
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true; # Enable XWayland support
-  };
-
   xdg.configFile."hypr" = {
     source = "${inputs.alexDotfiles}/hypr"; 
   };
