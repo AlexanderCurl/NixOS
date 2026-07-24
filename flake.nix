5f2f5456705bf0692440e3894664087ac5346d97
AlexanderCurl <alexc@alexc.hu>
2026-07-24 11:42:21 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     alexDotfiles = {
       url = "github:AlexanderCurl/Dotfiles";
       flake = false;
