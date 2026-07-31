40aa0c95b489e8d8abd2a47046b0e5b861891b2e
AlexanderCurl <alexc@alexc.hu>
2026-07-31 11:43:00 +0100

    Added caches

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,23 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # --- NIX SETTINGS & BINARY CACHES ---
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    
+    # URLs of the binary caches
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    
+    # Public keys required to verify the binaries
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   networking.hostName = "sasuke";
   networking.networkmanager.enable = true;
 
