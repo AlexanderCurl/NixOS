0b4cbe253f6e2ff98e18ce18eef93364254b25a8
AlexanderCurl <alexc@alexc.hu>
2026-08-02 09:14:45 +0100

    Cleanup, incorporate user packages into 1 flake

---
+++ modules/home/packages.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with.pkgs; [
+    brave-origin
+    discord
+    slack
+    kdePackages.kdeconnect-kde
+    openrazer-daemon
+    kitty-bin
+    fastfetch
+    mate-polkit
+    polychromatic
+    ffmpeg
+    nextcloud-client
+    ytmdesktop
+    teams-for-linux
+    teamspeak6-client
+    prismlauncher
+    easyeffects
+    nwg-look
+    nwg-displays
+    pavucontrol
+    hyprshot
+    hyprland-guiutils
+    hyprwayland-scanner
+    hyprland-qtutils
+    hyprcursor
+  ];
+}
